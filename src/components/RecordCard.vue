d531c250294ab1827bf189cf8a8c0416f0561c42
smathsp <sectriwind@gmail.com>
2026-05-25 19:02:24 +0800

    优化 RecordCard 组件的图片展示逻辑，简化无图时的占位符处理

--- src/components/RecordCard.vue
+++ src/components/RecordCard.vue
@@ -7,20 +7,10 @@
       <span class="card-date">{{ record.date }}</span>
       <span class="card-id">{{ record.id }}</span>
     </div>
-    <div class="card-images">
-      <template v-if="!record.images || record.images.length === 0">
-        <div class="card-img-slot">
-          <div class="img-placeholder">
-            <div class="cross"><div class="ring"></div></div>
-            <span>暂无截图</span>
-          </div>
-        </div>
-      </template>
-      <template v-else>
-        <div v-for="(src, j) in record.images" :key="j" class="card-img-slot has-image">
-          <img :src="src" alt="截图" loading="lazy">
-        </div>
-      </template>
+    <div v-if="record.images && record.images.length" class="card-images">
+      <div v-for="(src, j) in record.images" :key="j" class="card-img-slot has-image">
+        <img :src="src" alt="截图" loading="lazy">
+      </div>
     </div>
     <div v-if="record.note" class="card-note">{{ record.note }}</div>
   </article>
